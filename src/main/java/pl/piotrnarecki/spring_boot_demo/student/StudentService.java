3d7a454e4396cdfab0e9264a565c9f265c300fb6
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-07-21 12:21:19 +0200

    added db part 2

--- src/main/java/pl/piotrnarecki/spring_boot_demo/student/StudentService.java
+++ src/main/java/pl/piotrnarecki/spring_boot_demo/student/StudentService.java
@@ -1,6 +1,6 @@
 package pl.piotrnarecki.spring_boot_demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,8 +10,15 @@ import java.util.List;
 @Service // @Component
 public class StudentService  {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Mary", "mary@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
+        return studentRepository.findAll();
     }
 
 
