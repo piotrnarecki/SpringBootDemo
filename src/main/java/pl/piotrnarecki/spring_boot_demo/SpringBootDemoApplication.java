3044f8efc2f7fae2db96b0b482af7c43e9937d70
piotrnarecki <nareckipiotraleksander@gmail.com>
2021-07-21 09:35:10 +0200

    added dependencies injection

--- src/main/java/pl/piotrnarecki/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/pl/piotrnarecki/spring_boot_demo/SpringBootDemoApplication.java
@@ -2,16 +2,9 @@ package pl.piotrnarecki.spring_boot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pl.piotrnarecki.spring_boot_demo.student.Student;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
@@ -19,9 +12,6 @@ public class SpringBootDemoApplication {
     }
 
 
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(new Student(1L, "Mary", "mary@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
-    }
+
 
 }
